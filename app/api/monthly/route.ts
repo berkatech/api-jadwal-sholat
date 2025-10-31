e3c7943f7ed862bf01dba6ad3bd213a382753299
Nasrul Faizin <faizinnasrul21@gmail.com>
2025-10-31 22:29:17 +0700

    refactor(monthly): handle error by try catch

--- app/api/monthly/route.ts
+++ app/api/monthly/route.ts
@@ -4,38 +4,46 @@ import { NextRequest } from 'next/server';
 export async function GET(request: NextRequest) {
     const { searchParams } = request.nextUrl;
 
-    const kemenagResponse = await getSchedules({
-        province_id: searchParams.get("province_id"),
-        city_id: searchParams.get("city_id"),
-        month: searchParams.get("month"),
-        year: searchParams.get("year")
-    });
+    try {
+        const kemenagResponse = await getSchedules({
+            province_id: searchParams.get("province_id"),
+            city_id: searchParams.get("city_id"),
+            month: searchParams.get("month"),
+            year: searchParams.get("year")
+        });
 
-    const schedule = [];
+        const schedule = [];
 
-    for (const date in kemenagResponse.data) {
-        const row = kemenagResponse.data[date];
-        schedule.push({
-            date: date,
-            imsyak: row.imsak,
-            shubuh: row.subuh,
-            sunrise: row.terbit,
-            dhuha: row.dhuha,
-            dzuhur: row.dzuhur,
-            ashr: row.ashar,
-            maghrib: row.maghrib,
-            isya: row.isya,
-        })
-    }
-
-    const resp = {
-        message: "success",
-        data: {
-            province: kemenagResponse.prov,
-            city: kemenagResponse.kabko,
-            schedule: schedule,
+        for (const date in kemenagResponse.data) {
+            const row = kemenagResponse.data[date];
+            schedule.push({
+                date: date,
+                imsyak: row.imsak,
+                shubuh: row.subuh,
+                sunrise: row.terbit,
+                dhuha: row.dhuha,
+                dzuhur: row.dzuhur,
+                ashr: row.ashar,
+                maghrib: row.maghrib,
+                isya: row.isya,
+            })
         }
-    };
 
-    return Response.json(resp);
+        const resp = {
+            message: "success",
+            data: {
+                province: kemenagResponse.prov,
+                city: kemenagResponse.kabko,
+                schedule: schedule,
+            }
+        };
+        return Response.json(resp);
+    } catch (error) {
+        return Response.json({
+            message: "error",
+            error: error instanceof Error ? error.message : "failed to fetch schedules",
+        }, {
+            status: 500,
+        });
+    }
 }
