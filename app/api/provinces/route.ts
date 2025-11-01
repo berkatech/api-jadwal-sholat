cc62d81ef9e035488b9431df661c22705981f6c2
Nasrul Faizin <faizinnasrul21@gmail.com>
2025-11-01 13:15:54 +0700

    style(provinces): remove unused import

--- app/api/provinces/route.ts
+++ app/api/provinces/route.ts
@@ -1,9 +1,6 @@
-import got from "got";
-import { NextRequest } from "next/server";
-import { JSDOM } from 'jsdom';
 import { getProvinces } from "@/app/lib/adapter/bimaskemenag";
 
-export async function GET(request: NextRequest) {
+export async function GET() {
     const provinces = await getProvinces();
 
     return Response.json({
